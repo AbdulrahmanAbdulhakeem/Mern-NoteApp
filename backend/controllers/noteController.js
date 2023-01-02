10ec76f54cfb0b6d38db6b2cfefc6e1eb1ff428c
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2023-01-02 18:05:02 +0100

    Implemented DeleteNote Controller

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -38,39 +38,58 @@ const createNote = async (req, res) => {
 //@route PATCH /api/v1/note/:id
 //access Private
 const updateNote = async (req, res) => {
-    const {
-        user: { id: userId },
-        body: { title, note },
-        params: { id: noteId },
-      } = req;
-    
-      if (title === " " || note === " " || !title || !note) {
-        throw new BadRequestError("Input Cannot Be Empty");
-      }
-    
-      let updatedNote = await Note.findById(noteId);
-    
-      if (!updatedNote) {
-        throw new BadRequestError("Post Does Not Exist Or Has Been Deleted");
-      }
-    
-      if (updatedNote.createdBy.toString() !== userId) {
-        throw new UnAuthenticatedError("UnAuthorized Access");
-      }
-    
-      updatedNote = await Note.findByIdAndUpdate({ _id: noteId }, req.body, {
-        new: true,
-        runValidators: true,
-      });
-    
-      res.status(StatusCodes.OK).json(updatedNote);
+  const {
+    user: { id: userId },
+    body: { title, note },
+    params: { id: noteId },
+  } = req;
+
+  if (title === " " || note === " " || !title || !note) {
+    throw new BadRequestError("Input Cannot Be Empty");
+  }
+
+  let updatedNote = await Note.findById(noteId);
+
+  if (!updatedNote) {
+    throw new BadRequestError("Post Does Not Exist Or Has Been Deleted");
+  }
+
+  if (updatedNote.createdBy.toString() !== userId) {
+    throw new UnAuthenticatedError("UnAuthorized Access");
+  }
+
+  updatedNote = await Note.findByIdAndUpdate({ _id: noteId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(StatusCodes.OK).json(updatedNote);
 };
 
 //@desc deleteNote
 //@route DELETE /api/v1/note/:id
 //access Private
 const deleteNote = async (req, res) => {
-  res.send("Deleted");
+  const {
+    user: { id: userId },
+    params: { id: noteId },
+  } = req;
+
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    throw new BadRequestError("Post Does Not Exist Or Has Been Deleted");
+  }
+
+  if (note.createdBy.toString() !== userId) {
+    throw new UnAuthenticatedError("UnAuthorized Access");
+  }
+
+  await note.remove();
+
+  const notes = await Note.find();
+
+  res.status(StatusCodes.OK).json(notes);
 };
 
 module.exports = {
