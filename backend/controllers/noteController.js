4acc4d863c94aee34c9712f585b1da5d2a55073e
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2023-01-02 15:15:40 +0100

    Created Note Controller

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,30 @@
+const { response } = require('express')
+const express = require('express')
+
+const getAllNotes = async(req,res) => {
+    res.send('All Notes')
+}
+
+const getNoteById = async(req,res) => {
+    res.send('Get Note By Id')
+}
+
+const createNote = async(req,res) => {
+    res.send('Create Note')
+}
+
+const updateNote = async(req,res) => {
+    res.send('Updated Note')
+}
+
+const deleteNote = async(req,res) => {
+    res.send('Deleted')
+}
+
+module.exports = {
+    getAllNotes,
+    getNoteById,
+    createNote,
+    updateNote,
+    deleteNote
+}
