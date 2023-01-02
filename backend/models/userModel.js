b111a79b1748f024368f30089354e04b815eb67a
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2023-01-02 13:37:09 +0100

    Created User Schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please Provide Valid Name"],
+      minlength: 1,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please Provide Valid Email"],
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please provide a valid email",
+      ]
+    },
+    password: {
+      type: String,
+      required: [true, "Please Provide Valid Password"],
+      minlength: 1,
+      trim:true
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
