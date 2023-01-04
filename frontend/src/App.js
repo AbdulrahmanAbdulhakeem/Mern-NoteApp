db7628b0393be3f715fd578500578725dab2759f
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2023-01-04 22:32:57 +0100

    Created Index File In The Pages Directory For Clean Exporting Of Files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Header } from "./components";
 
 function App() {
