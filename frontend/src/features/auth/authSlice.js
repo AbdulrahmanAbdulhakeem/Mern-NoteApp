877aeae6530d96b0ca5fd76ebb1b683179cc75a0
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2023-01-04 22:26:17 +0100

    Created Register User Reducer

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -11,6 +11,24 @@ const initialState = {
   message: "",
 };
 
+
+//Register User
+export const register = createAsyncThunk(
+    "auth/register",
+    async (user, thunkAPI) => {
+      try {
+        return await authService.register(user);
+      } catch (error) {
+        const message =
+          (error.message && error.response.data && error.response.data.message) ||
+          error.message ||
+          error.toString();
+        return thunkAPI.rejectWithValue(message);
+      }
+    }
+  );
+
+  
 export const authSlice = createSlice({
     name: "auth",
     initialState,
